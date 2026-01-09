1c4c599a04606c83c65fa66bb4ca6be75e9df08c
Arden <arden.haldorson@gmail.com>
2026-01-09 01:11:49 +0100

    Fix production image paths and add Netlify redirects

--- src/constants/slotSymbols.ts
+++ src/constants/slotSymbols.ts
@@ -1,48 +1,29 @@
+import jokerImg from "@/assets/images/slotImages/joker.png";
+import jackImg from "@/assets/images/slotImages/jack.png";
+import queenImg from "@/assets/images/slotImages/queen.png";
+import kingImg from "@/assets/images/slotImages/king.png";
+import cherryImg from "@/assets/images/slotImages/cherry.png";
+import lemonImg from "@/assets/images/slotImages/lemon.png";
+import bellImg from "@/assets/images/slotImages/bell.png";
+import sevenImg from "@/assets/images/slotImages/seven.png";
+import starImg from "@/assets/images/slotImages/star.png";
+import dollarImg from "@/assets/images/slotImages/dollar.png";
+import diamondImg from "@/assets/images/slotImages/diamond.png";
+import crownImg from "@/assets/images/slotImages/crown.png";
+
 const slotSymbols = [
-	{ id: 0, src: "src/assets/images/slotImages/joker.png", alt: "Joker" },
-	{
-		id: 1,
-		src: "src/assets/images/slotImages/jack.png",
-		alt: "Jack",
-	},
-	{
-		id: 2,
-		src: "src/assets/images/slotImages/queen.png",
-		alt: "Queen",
-	},
-	{
-		id: 3,
-		src: "src/assets/images/slotImages/king.png",
-		alt: "King",
-	},
-	{
-		id: 4,
-		src: "src/assets/images/slotImages/cherry.png",
-		alt: "Cherry",
-	},
-	{ id: 5, src: "src/assets/images/slotImages/lemon.png", alt: "Lemon" },
-	{ id: 6, src: "src/assets/images/slotImages/bell.png", alt: "Bell" },
-	{
-		id: 7,
-		src: "src/assets/images/slotImages/seven.png",
-		alt: "Seven",
-	},
-	{ id: 8, src: "src/assets/images/slotImages/star.png", alt: "Star" },
-	{
-		id: 9,
-		src: "src/assets/images/slotImages/dollar.png",
-		alt: "Dollar",
-	},
-	{
-		id: 10,
-		src: "src/assets/images/slotImages/diamond.png",
-		alt: "Diamond",
-	},
-	{
-		id: 11,
-		src: "src/assets/images/slotImages/crown.png",
-		alt: "Crown",
-	},
+	{ id: 0, src: jokerImg, alt: "Joker" },
+	{ id: 1, src: jackImg, alt: "Jack" },
+	{ id: 2, src: queenImg, alt: "Queen" },
+	{ id: 3, src: kingImg, alt: "King" },
+	{ id: 4, src: cherryImg, alt: "Cherry" },
+	{ id: 5, src: lemonImg, alt: "Lemon" },
+	{ id: 6, src: bellImg, alt: "Bell" },
+	{ id: 7, src: sevenImg, alt: "Seven" },
+	{ id: 8, src: starImg, alt: "Star" },
+	{ id: 9, src: dollarImg, alt: "Dollar" },
+	{ id: 10, src: diamondImg, alt: "Diamond" },
+	{ id: 11, src: crownImg, alt: "Crown" },
 ];
 
 export default slotSymbols;
